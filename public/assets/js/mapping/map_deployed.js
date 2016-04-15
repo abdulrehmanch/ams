260453062eefa94889700b4e8015aaff6eed5b93
Abdul Rehman <abdulrehman.ch@live.com>
2016-04-15 16:31:31 +0500

    -- map view and location

---
+++ public/assets/js/mapping/map_deployed.js
@@ -0,0 +1,95 @@
+// map
+mapCenter = new L.LatLng(31.579006, 73.402031);
+var map = new L.Map('map', {
+    center: mapCenter,
+    zoom: 6,
+    zoomControl: false
+});
+// Zoom Control
+map.addControl(new L.Control.Zoom({
+    position: 'topleft'
+}));
+L.control.navbar().addTo(map);
+
+var esriTopo = L.esri.basemapLayer("Topographic");
+var esriImagery = L.esri.basemapLayer('Imagery');
+//map.addLayer(esriTopo);
+
+var waterQualityLyer = L.esri.dynamicMapLayer({
+    url: 'http://202.166.168.188/arcgis/rest/services/Punjab/PB_Saffpani_WQ_layers_v_08042016/MapServer'
+});
+
+
+var gTerrain = new L.Google('TERRAIN');
+var gSatellite = new L.Google('SATELLITE');
+var gHybrid = new L.Google('HYBRID');
+var gStreet = new L.Google('ROADMAP');
+map.addLayer(gTerrain);
+
+// Layer controller
+var baseLayers =
+{
+    "TOPOGRPHIC": gTerrain,
+    "ESRI TOPOGRPHIC":esriTopo,
+    "Satellite": gSatellite,
+    "STREET MAP":gStreet,
+    "HYBRID MAP":gHybrid,
+    "ESRI EMAGERY":esriImagery
+};
+var overlays = {
+    "Water Quality": waterQualityLyer
+};
+L.control.layers(baseLayers, overlays).addTo(map);
+
+var geoJsonData = $.getJSON('map/mapdata');
+console.log(geoJsonData);
+geoJsonData.then(function(data)
+{
+    L.geoJson(data,
+        {
+            onEachFeature: onEachFeature
+        }).addTo(map);
+});
+// on each feature
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties) {
+        var popupContent = '';
+        layer.bindLabel('' +feature.properties.count,
+            {
+                noHide:true,
+                className: "my-label",
+                offset: [-15, -42]
+            });
+        popupContent += "Scheme Name: "+ feature.properties.scheme_name + "<br>"
+            + "No. of Assets Deplyed: "+ feature.properties.count + "<br>";
+
+        layer.bindPopup(popupContent);
+    }
+}
+
+//Zoom to location
+function zoomToCoord(lat, lon){
+
+    // map.panTo([lat, lon]);
+    map.setView([lat, lon], 16)
+    console.log(lat);
+    console.log(lon);
+
+}
+//
+////  Navgation pan on Map
+////var Navgation = L.control({position: 'bottomleft'});
+////Navgation.onAdd = function (map) {
+////    this._div = L.DomUtil.create('div', 'mapNavigation'); // create a div with a class "info"
+////    this.update();
+////    return this._div;
+////};
+//
+////Navgation.update = function () {
+////
+////    this._div.innerHTML = "";
+////
+////};
+//// Navgation.addTo(map);
